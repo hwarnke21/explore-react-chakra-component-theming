29e996cbe95649ee116842dfc416852440eac25b
myuut <hwarnke21@gmail.com>
2022-10-12 15:07:26 -0500

    feat: add input component theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,9 +1,11 @@
 import { extendTheme } from '@chakra-ui/react';
 import Menu from './components/Menu';
+import { inputTheme } from './components/Input';
 
 const theme = extendTheme({
   components: {
     Menu,
+    Input: inputTheme,
   },
 });
 
