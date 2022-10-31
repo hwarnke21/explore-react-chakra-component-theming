4564c8bf6bd544e28a114306cc8e048ae334b77b
myuut <hwarnke21@gmail.com>
2022-10-31 13:51:49 -0500

    feat: update placement and center layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   ChakraProvider,
   Box,
+  Center,
   SimpleGrid,
   GridItem,
   Icon,
@@ -16,63 +17,65 @@ export default function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box position="relative" h="100vh" p={12}>
-        <SimpleGrid columns={[1, 1, 1, 2, 3]} spacing="12">
-          <GridItem
-            border="1px solid"
-            borderRadius="xl"
-            p={8}
-            _light={{ bg: 'gray.200' }}
-            _dark={{ bg: 'gray.700' }}
-          >
-            <Text display="inline-flex" alignItems="baseline">
-              Hover the icon for more information.
-              <Tooltip
-                label="Tooltip with new theme defaults of brand color scheme and medium size."
-                placement="top"
-              >
-                <Icon as={QuestionIcon} mx={2} color="brand.400" />
-              </Tooltip>
-            </Text>
-          </GridItem>
-          <GridItem
-            border="1px solid"
-            borderRadius="xl"
-            p={8}
-            _light={{ bg: 'gray.200' }}
-            _dark={{ bg: 'gray.700' }}
-          >
-            <Text display="inline-flex" alignItems="baseline">
-              Hover the icon for more information.
-              <Tooltip
-                label="Tooltip with blue color scheme and new large size."
-                placement="bottom-end"
-                colorScheme="blue"
-                size="lg"
-              >
-                <Icon as={QuestionIcon} mx={2} color="blue.400" />
-              </Tooltip>
-            </Text>
-          </GridItem>
-          <GridItem
-            border="1px solid"
-            borderRadius="xl"
-            p={8}
-            _light={{ bg: 'gray.200' }}
-            _dark={{ bg: 'gray.700' }}
-          >
-            <Text display="inline-flex" alignItems="baseline">
-              Hover the icon for more information.
-              <Tooltip
-                label="Tooltip with green color scheme and new small size."
-                placement="top-start"
-                colorScheme="green"
-                size="sm"
-              >
-                <Icon as={QuestionIcon} mx={2} color="green.400" />
-              </Tooltip>
-            </Text>
-          </GridItem>
-        </SimpleGrid>
+        <Center>
+          <SimpleGrid columns={[1, 1, 1, 2, 3]} spacing="12">
+            <GridItem
+              border="1px solid"
+              borderRadius="xl"
+              p={8}
+              _light={{ bg: 'gray.200' }}
+              _dark={{ bg: 'gray.700' }}
+            >
+              <Text display="inline-flex" alignItems="baseline">
+                Hover the icon for more information.
+                <Tooltip
+                  label="Tooltip with new theme defaults of brand color scheme and medium size."
+                  placement="top"
+                >
+                  <Icon as={QuestionIcon} mx={2} color="brand.400" />
+                </Tooltip>
+              </Text>
+            </GridItem>
+            <GridItem
+              border="1px solid"
+              borderRadius="xl"
+              p={8}
+              _light={{ bg: 'gray.200' }}
+              _dark={{ bg: 'gray.700' }}
+            >
+              <Text display="inline-flex" alignItems="baseline">
+                Hover the icon for more information.
+                <Tooltip
+                  label="Tooltip with blue color scheme and new large size."
+                  placement="top"
+                  colorScheme="blue"
+                  size="lg"
+                >
+                  <Icon as={QuestionIcon} mx={2} color="blue.400" />
+                </Tooltip>
+              </Text>
+            </GridItem>
+            <GridItem
+              border="1px solid"
+              borderRadius="xl"
+              p={8}
+              _light={{ bg: 'gray.200' }}
+              _dark={{ bg: 'gray.700' }}
+            >
+              <Text display="inline-flex" alignItems="baseline">
+                Hover the icon for more information.
+                <Tooltip
+                  label="Tooltip with green color scheme and new small size."
+                  placement="top"
+                  colorScheme="green"
+                  size="sm"
+                >
+                  <Icon as={QuestionIcon} mx={2} color="green.400" />
+                </Tooltip>
+              </Text>
+            </GridItem>
+          </SimpleGrid>
+        </Center>
         <ColorModeSwitcher />
       </Box>
     </ChakraProvider>
